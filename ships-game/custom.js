3d2c157d52fe5f9d5c3629dfe316c37c71b844d0
LukaszBobrowski <lukasz@bobrowski.biz>
2017-08-24 20:23:37 +0200

    start game state and location

---
+++ ships-game/custom.js
@@ -0,0 +1,17 @@
+var location1,
+	location2,
+	location3,
+	guess,
+	guesses,
+	hits,
+	isSunk;
+
+// start game state
+	hits = 0;
+	guesses = 0;
+	isSunk = false;
+
+// first ship - "3 elemnts build"  - location:
+	location1 = 3;
+	location2 = 4;
+	location3 = 5;
