28480a985ab3a5651095d7ad006b11a31d2868a3
LukaszBobrowski <lukasz@bobrowski.biz>
2017-08-30 19:06:00 +0200

    game easy logic

--- ships-game/custom.js
+++ ships-game/custom.js
@@ -11,7 +11,31 @@ var location1,
 	guesses = 0;
 	isSunk = false;
 
-// first ship - "3 elemnts build"  - location:
+// first ship - "Cruiser 3-elemnts class of ships"  - location:
 	location1 = 3;
 	location2 = 4;
 	location3 = 5;
+
+while (isSunk == false) {
+	guess = prompt('Search: Cruiser(3-size) class of ships. Enter position to shot(from 0-6): ');
+	console.log(guess);
+	if (guess < 0 || guess > 6) {
+		alert('it is not number from 0-6 pleas enter corect position.');
+	} else {
+		guesses = guesses + 1;
+
+		if 	(guess == location1 || guess == location2 || guess == location3) {
+			hits = hits + 1;
+			alert("shot corect");
+
+			if (hits == 3) {
+				isSunk = true;
+				alert("Ship die");
+			}
+		} else {
+			alert('fail shot');
+		}
+	}
+}
+var stats = "You need " + guesses + " shots to destroy cruiser(3-size) ship. Your effective is: " + (3/guesses) + ".";
+alert(stats);
